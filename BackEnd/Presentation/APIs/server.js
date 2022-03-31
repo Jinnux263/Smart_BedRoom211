d7601d93dac294efe03f95eab9e145b53c7bdc0f
Jinnux <nguyentandat26032001@gmail.com>
2022-03-31 21:34:25 +0700

    up

--- BackEnd/Presentation/APIs/server.js
+++ BackEnd/Presentation/APIs/server.js
@@ -140,18 +140,19 @@ AdafruitController.updateDatabase()
 // systemInit()
 
 setInterval(async function() {
-  handleNewData()
+  console.log("Start update Database...");
+  AdafruitController.updateDatabase()
 }, the_interval);
 
-function handleNewData (workerData) {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker(__dirname + '/worker.js', { workerData });
+// function handleNewData (workerData) {
+//   return new Promise((resolve, reject) => {
+//     const worker = new Worker(__dirname + '/worker.js', { workerData });
 
-    worker.on('message', resolve);
-    worker.on('error', reject);
-    worker.on('exit', (code) => {
-      if (code !== 0)
-        reject(new Error(`stopped with  ${code} exit code`));
-    });
-  })
-}
+//     worker.on('message', resolve);
+//     worker.on('error', reject);
+//     worker.on('exit', (code) => {
+//       if (code !== 0)
+//         reject(new Error(`stopped with  ${code} exit code`));
+//     });
+//   })
+// }
